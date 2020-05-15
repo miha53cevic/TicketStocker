9f8e35d647ab3f2c487fbce6164948a3309961d1
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2020-05-15 13:11:19 +0200

    finish backend

--- responsive/src/ticket/ticket_info.php
+++ responsive/src/ticket/ticket_info.php
@@ -1,6 +1,15 @@
 <?php
 
+$id = $_GET['id'] | 0;
 
+require_once '../includes/utils.php';
+$handler = createSqlHandler();
+$handler->querry("SELECT ticket.name, ticket.price, times.time1, times.time2, times.time3, seats.row, seats.col FROM ticket, times, seats WHERE $id = ticket.id AND ticket.id = times.id AND ticket.id = seats.id", true);
+
+// Check for case if the ticket by requested ID doesn't exist
+if (!sql_data_exists($handler)) {
+    echo '<h1 style="color: white;">Error: could not find given ticket!</h1>';
+} 
 
 ?>
 
@@ -33,8 +42,8 @@
         </header>
 
         <div class="container transparent align-items-center">
-            <div class="container w100 align-items-center">
-                <h1 class="hHeader"> Star Wars III </h1>
+            <div class="container w100 align-items-center ticket-pannel" style="background-image: url('../../images/<?php echo $_GET['id'] ?>.jpg');">
+                <h1 class="hHeader text-outline"> <?php echo $handler->data[0]['name']; ?> </h1>
             </div>
             <div class="row">
                 <div class="container w100 align-items-center">
@@ -42,17 +51,54 @@
                         <div class="col" id="time-list">
                             <i class="pHeader">Available times</i>
                             <hr>
-                        </div>
+                                <?php  
+                                    
+                                    $time1 = $handler->data[0]['time1'];
+                                    $time2 = $handler->data[0]['time2'];
+                                    $time3 = $handler->data[0]['time3'];
 
-                    </div> <!-- Content -->
-                </div> <!-- New container -->
+                                    $noTime = '00:00:00';
+                                    if ($time1 != $noTime) {
+                                        echo "<p class='pText pHover'>$time1</p>";
+                                        echo "<hr>";
+                                    }
 
-                <div class="container w100 align-items-center">
-                    <div class="content">
+                                    if ($time2 != $noTime) {
+                                        echo "<p class='pText pHover'>$time2</p>";
+                                        echo "<hr>";
+                                    }
+                                    
+                                    if ($time3 != $noTime) {
+                                        echo "<p class='pText pHover'>$time3</p>";
+                                    }
+                                        
+                                ?>
+                                </div>
+
+                        </div> <!-- Content -->
+                    </div> <!-- New container -->
+
+                    <div class="container w100 align-items-center">
+                        <div class="content">
                         <div class="col">
                             <div id="canvasArea"></div><br>
-                            <p class="pHeader">Time: <span id="time">11:00</span> </p>
-                            <p class="pHeader">Seat: <span id="seat">5</span> </p><br>
+                            <p class="pHeader">Time: <span id="time">none selected</span> </p>
+                            <hr>
+                            
+                            <?php 
+                            
+                                if ($handler->data[0]['row'] != 0 && $handler->data[0]['col'] != 0) {
+                                    printf('<p class="pHeader">Seat: <span id="seat">1 row, 1 col</span> </p><br>');
+                                    printf('<div class="row justify-content-center">');
+                                    printf('<p class="pHeader">Row:</p>');
+                                    printf('<input class="text-field" min="1" max="%s" style="width: 3.5rem;" type="number" value="1" id="seat_row">', $handler->data[0]['row']);
+                                    printf('<p class="pHeader">Column:</p>');
+                                    printf('<input class="text-field" min="1" max="%s" style="width: 3.5rem;" type="number" value="1" id="seat_col">', $handler->data[0]['col']);
+                                    printf('</div><br>');
+                                }
+                            
+                            ?>
+
                             <div class="row justify-content-center">
                                 <p class="pHeader">Amount:</p>
                                 <input class="text-field" min="1" max="5" style="width: 3.5rem;" type="number" value="1">
@@ -77,11 +123,9 @@
 
         <br><br><br><br><br><br><br><br><br><br><br><br>
 
-        <footer class="footer">
-            <h1>Contact</h1>
-            <p>e-mail: miha53cevic</p>
-            <p>tel: 123 456 789</p>
-        </footer>
+        <?php 
+            include '../includes/footer.php';
+        ?>
 
     </main>
 
@@ -90,14 +134,22 @@
 <script>
 
     // Setup time choosing
-    const times = ['10:00', '13:00', '16:00', '19:00'];
-    times.forEach(element => {
-        $("#time-list").append(`<p class="pText pHover">${element}</p>`);
-        $("#time-list").append("<hr>");
-
+    for (let i = 0; i < 3; i++) {
         $("#time-list").on('click', 'p', function() {
             $("#time").text($(this).text());
         });
+    }
+
+    let seatRow = 1;
+    let seatCol = 1;
+    $("#seat_row").on('click', function() {
+        seatRow = $(this).val();
+        $("#seat").text(`${seatRow} row, ${seatCol} col`);
+    });
+
+    $("#seat_col").on('click', function() {
+        seatCol = $(this).val();
+        $("#seat").text(`${seatRow} row, ${seatCol} col`);
     });
 
 </script>
